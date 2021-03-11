d7a034035faa25cd6a65f8fcc1f264adada694c7
Iryna Rudzko <irynarudzko@MacBook-Air-Iryna.local>
2021-03-11 08:36:23 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    return parseFloat(n.toString().split('').reverse().join(''));
 }
+
+//solution:
+//https://www.freecodecamp.org/news/js-basics-how-to-reverse-a-number-9aefc20afa8d/
+
+// * Math.sign(n) – если нужно сохранить отрицательное значение
